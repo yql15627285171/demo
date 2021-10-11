b1889c6a90d6eae371f58b5d3d6d4b004cf38957
yuqiliang <502531008@qq.com>
2021-10-11 17:54:28 +0800

    第一次上传

---
+++ App.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show'),
+			uni.onTabBarMidButtonTap(()=>{
+			        console.log("点击了");
+			 });
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	/*每个页面公共css */
+	@import "uview-ui/index.scss";
+</style>
+ 
---
+++ pages/index/index.vue
@@ -0,0 +1,59 @@
+<template>
+	<view class="content" :class="title">
+		nihaop
+		<live></live>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			console.log("page load")
+		},
+		onShow(){
+			console.log("page show")
+		},
+		onReady(){
+			console.log("page ready")
+		},
+		onUnload(){
+			console.log("page onUnload")
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
